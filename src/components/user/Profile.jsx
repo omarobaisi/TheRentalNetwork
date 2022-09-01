6e743a0ddc0f1b55e57b6a037c5e5a8832b32159
Mohammad <hasaeen3252@gmail.com>
2022-09-01 12:43:28 +0300

    started profile

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 function Profile(props) {
+  
     return (
         <div>
-            
-        </div>
+        <Link to="/" className="btn btn-primary">Review</Link>
+            </div>
     );
-}
+    }
 
 export default Profile;
