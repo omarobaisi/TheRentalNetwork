be43307695489becdd03ca34e28196736377f620
Mohammad <hasaeen3252@gmail.com>
2022-09-04 10:24:33 +0300

    review comp

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom'
-import { useNavigate } from "react-router-dom";
 
 function Profile(props) {
   
     return (
         <div>
-        <Link to="/" className="btn btn-primary">Review</Link>
-            </div>
+        <Link to="/review" className="btn btn-primary">Review</Link>
+         </div>
     );
     }
 
--- src/components/user/Review.jsx
+++ src/components/user/Review.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import axios from 'axios'
+import React, {useState} from 'react'
+import {Rating} from 'react-simple-star-rating'
 
 function Review(props) {
+    const [rate, setRating] = useState(0)
+    const [body, setReviewText] = useState("")
+    let review = {
+        rate,
+        body
+    }
+    const  handleSubmit = async(evt)=> {
+        evt.preventDefault();
+       await axios.post('http://localhost:4000/review', review)
+        console.log(review)
+    }
+    const handleRating = (rate) => {
+        setRating(rate / 20)
+    }
+
     return (
-        <div>
-            
+        <div className='review-container'>
+            <div className='reviewer'>Name</div>
+            <Rating onClick={handleRating}
+                ratingValue={rate}/>
+                <div>
+                <textarea name="reviewTextArea" type="text" cols="80" rows="5"
+                value={body}
+                onChange={
+                    e => setReviewText(e.target.value)
+            }></textarea>
+                </div>
+                
+            <button onClick={handleSubmit}> Submit</button>
         </div>
-    );
+    )
 }
 
 export default Review;
