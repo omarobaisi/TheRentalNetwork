eb0d5b509311dc6efa6b0755ab66c4da6b6238df
Tamara-Daghlis <daghlistamara@gmail.com>
2022-09-06 15:51:01 -0700

    add history component

--- server/Services/user-services.js
+++ server/Services/user-services.js
@@ -1,72 +1,97 @@
-const User = require('../Model/user');
+const User = require("../Model/user");
+const Record = require("../Model/rentRecord");
 
 module.exports.getUsers = async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(e) {
-        res.status(404).json({ message: "Coudn't find users", error: e })
-    }
-}
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(404).json({ message: "Coudn't find users", error: e });
+  }
+};
 
 module.exports.getUserById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await User.findById(id)
-        res.send(user)
-    } catch(e) {
-        res.status(404).json({ message: "Coudn't find The user", error: e })
-    }
-}
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.send(user);
+  } catch (e) {
+    res.status(404).json({ message: "Coudn't find The user", error: e });
+  }
+};
 
 module.exports.getCurrentUser = async (req, res) => {
-    if(req.user) {
-        res.send(req.user);
-    } else {
-        res.status(401).json({ success: false, message: "Not logged in" })
-    }
-}
+  if (req.user) {
+    res.send(req.user);
+  } else {
+    res.status(401).json({ success: false, message: "Not logged in" });
+  }
+};
 
 module.exports.register = async (req, res) => {
-    try {
-        const { password } = req.body;
-        const { user } = req.body;
-        const newUser = new User(user);
-        const registeredUser = await User.register(newUser, password);
-        req.login(registeredUser, err => {
-            if(err) res.status(404).json({ message: "Coudn't login", error: e })
-            res.send(req.user)
-        })
-    } catch(e) {
-        res.status(404).json({ message: "Coudn't register", error: e })
-    }
-}
+  try {
+    const { password } = req.body;
+    const { user } = req.body;
+    const newUser = new User(user);
+    const registeredUser = await User.register(newUser, password);
+    req.login(registeredUser, (err) => {
+      if (err) res.status(404).json({ message: "Coudn't login", error: e });
+      res.send(req.user);
+    });
+  } catch (e) {
+    res.status(404).json({ message: "Coudn't register", error: e });
+  }
+};
 
 module.exports.login = async (req, res) => {
-    res.send(req.user);
-}
+  res.send(req.user);
+};
 
 module.exports.logout = async (req, res) => {
-    req.logout();
-}
+  req.logout();
+};
 
 module.exports.updateUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const newUser = req.body
-        const updatedUser = await User.findByIdAndUpdate(id, newUser, { new: true, runValidators: true } )
-        res.send(updatedUser);
-    } catch(e) {
-        res.status(404).json({ message: "Coudn't update the user", error: e })
-    }
-}
+  try {
+    const { id } = req.params;
+    const newUser = req.body;
+    const updatedUser = await User.findByIdAndUpdate(id, newUser, {
+      new: true,
+      runValidators: true,
+    });
+    res.send(updatedUser);
+  } catch (e) {
+    res.status(404).json({ message: "Coudn't update the user", error: e });
+  }
+};
 
 module.exports.deleteUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedUser = await User.findByIdAndDelete(id, { new: true })
-        res.send(deletedUser);
-    } catch(e) {
-        res.status(404).json({ message: "Coudn't delete the user", error: e })
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id, { new: true });
+    res.send(deletedUser);
+  } catch (e) {
+    res.status(404).json({ message: "Coudn't delete the user", error: e });
+  }
+};
+
+module.exports.userRecord = async (req, res) => {
+  const userId = req.params.id;
+  const records = await Record.find()
+    .populate({
+      path: "product",
+      populate: [
+        {
+          path: "owner",
+        },
+      ],
+    })
+    .populate("renter");
+  recordsArray = [];
+  records.forEach((record) => {
+    if (record.renter._id == userId) {
+      recordsArray.push(record);
     }
-}
+  });
+  res.send(recordsArray);
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import axios from 'axios'
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
 
-import NavBar from './components/navbar/Navbar';
-import Landing from './components/landing/Landing';
-import Show from './components/product/show/Show';
-import Edit from './components/product/Edit';
-import Rent from './components/product/Rent';
-import Login from './components/user/Login'
-import Register from './components/user/Register'
-import Profile from './components/user/Profile'
-import Review from './components/user/Review'
-import AddProduct from './components/product/AddProduct';
-import Records from './components/records/Records';
+import NavBar from "./components/navbar/Navbar";
+import Landing from "./components/landing/Landing";
+import Show from "./components/product/show/Show";
+import Edit from "./components/product/Edit";
+import Rent from "./components/product/Rent";
+import Login from "./components/user/Login";
+import Register from "./components/user/Register";
+import Profile from "./components/user/Profile";
+import Review from "./components/user/Review";
+import AddProduct from "./components/product/AddProduct";
+import Records from "./components/records/Records";
+import History from "./components/user/History";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("");
 
-  const [currentUser, setCurrentUser] = useState('');
-
-  const newCurrentUser = user => {
+  const newCurrentUser = (user) => {
     setCurrentUser(user);
-  }
+  };
 
   return (
     <div>
-      
       <NavBar currentUser={currentUser} newCurrentUser={newCurrentUser} />
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/product/:id/show' element={<Show />} />
-        <Route path='/product/:id/new' element={<Edit />} />
-        <Route path='/product/:id/rent' element={<Rent />} />
-        <Route path='/product/add' element={<AddProduct currentUser={currentUser} />} />
-        <Route path='/records' element={<Records />} />
-        <Route path='/login' element={<Login newCurrentUser={newCurrentUser} />} />
-        <Route path='/register' element={<Register newCurrentUser={newCurrentUser} />} />
-        <Route path='/profile/:id' element={<Profile />} />
-        <Route path='/review/:userId' element={<Review />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/product/:id/show" element={<Show />} />
+        <Route path="/product/:id/new" element={<Edit />} />
+        <Route path="/product/:id/rent" element={<Rent />} />
+        <Route
+          path="/product/add"
+          element={<AddProduct currentUser={currentUser} />}
+        />
+        <Route path="/records" element={<Records />} />
+        <Route
+          path="/login"
+          element={<Login newCurrentUser={newCurrentUser} />}
+        />
+        <Route
+          path="/register"
+          element={<Register newCurrentUser={newCurrentUser} />}
+        />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/review/:userId" element={<Review />} />
+        <Route path="/:userId/history" element={<History />} />
       </Routes>
     </div>
   );
