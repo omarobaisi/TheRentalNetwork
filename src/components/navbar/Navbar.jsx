1bc647a2f6327487bf1aed2df9841b92f3229a8b
AnasAttar <anasattar2010@gmail.com>
2022-09-07 16:24:15 +0300

    use joi

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,61 +1,73 @@
-import React, {useState, useEffect} from 'react';
-import axios from "axios"
-import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import "./NavBar.css"
-import logo from "./Logo.png"
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./NavBar.css";
+import logo from "./Logo.png";
 
 function NavBar(props) {
+  const logout = () => {
+    axios.post("http://localhost:4000/user/logout");
+    props.newCurrentUser("");
+  };
 
-    const logout = async () => {
-        await axios.post("http://localhost:4000/user/logout");
-        props.newCurrentUser('');
-    }
+  const getCurrentUser = async () => {
+    try {
+      const user = await axios.get("http://localhost:4000/user/current");
+      props.newCurrentUser(user.data);
+    } catch (e) {}
+  };
 
-    const getCurrentUser = async () => {
-        try {
-            const user = await axios.get("http://localhost:4000/user/current");
-            props.newCurrentUser(user.data);
-        } catch(e) {
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
 
-        }
-    }
-
-    useEffect(() => {
-        getCurrentUser();
-      }, []);
-
-    return (
-        <Navbar expand="md">
-        <Container>
-            <Link className='Nav-Link' to={"/"}><Navbar.Brand><img className='Nav-Logo' src={logo} alt=""></img></Navbar.Brand></Link>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Link className='Nav-Link' to={"/"} key={"home"}>Home</Link>
-                <Link className='Nav-Link' to={"/product/add"} key={"add"}>New Product</Link>
-            </Nav>
-            <Nav>
-
-            </Nav>
-            {props.currentUser !== '' ? (
-                [
-                    <Link className='Nav-Link' to={`/profile/${props.currentUser._id}`} key={"profile"}>{props.currentUser.name}</Link>,
-                    <div className='Nav-Link' onClick={logout} >Logout</div>
-                ]
-            ) : (
-                [
-                    <Link className='Nav-Link' to={"/login"} key={"login"}>Login</Link>,
-                    <Link className='Nav-Link' to={"/register"} key={"register"}>Register</Link>,
-                ]
-            )}
-            </Navbar.Collapse>
-        </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar expand="md">
+      <Container>
+        <Link className="Nav-Link" to={"/"}>
+          <Navbar.Brand>
+            <img className="Nav-Logo" src={logo} alt=""></img>
+          </Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link className="Nav-Link" to={"/"} key={"home"}>
+              Home
+            </Link>
+            <Link className="Nav-Link" to={"/product/add"} key={"add"}>
+              New Product
+            </Link>
+          </Nav>
+          <Nav></Nav>
+          {props.currentUser !== ""
+            ? [
+                <Link
+                  className="Nav-Link"
+                  to={`/profile/${props.currentUser._id}`}
+                  key={"profile"}
+                >
+                  {props.currentUser.name}
+                </Link>,
+                <div className="Nav-Link" onClick={logout}>
+                  Logout
+                </div>
+              ]
+            : [
+                <Link className="Nav-Link" to={"/login"} key={"login"}>
+                  Login
+                </Link>,
+                <Link className="Nav-Link" to={"/register"} key={"register"}>
+                  Register
+                </Link>
+              ]}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
--- src/components/product/Add.jsx
+++ src/components/product/Add.jsx
@@ -1,93 +1,164 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { useNavigate, Navigate } from 'react-router-dom';
-import "./product.css"
+import axios from "axios";
+import Joi from "joi";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate, Navigate } from "react-router-dom";
+import "./product.css";
 
-function AddProduct({currentUser}) {
+function AddProduct({ currentUser }) {
+  const navigate = useNavigate();
+  let [errorList, SetErrorList] = useState([]);
+  //   const [product, setProduct] = useState({});
 
-    const navigate = useNavigate();
-    const [images , setImages] =useState([])
-    const [product ,setProduct] =useState({})
+  let [product, setProduct] = useState({
+    name: "",
+    price: 0,
+    category: "",
+    description: "",
+    Email: "",
+    Password: ""
+  });
 
+  const navigateFun = useNavigate();
 
-    useEffect(()=>{
+  function goToPage(path) {
+    navigateFun("/" + path);
+  }
 
-        insertPost()
-
-    } , [product]) 
-
-
-    const fileSelectedHandler = async (e) =>{
-        let files = e.target.files
-        for (const image of files) {
-             images.push(image)
-        }
-        setImages(images)
+  const submitForm = (e) => {
+    e.preventDefault();
+    let ValidationResult = ValidationForm();
+    if (ValidationResult.error) {
+      ValidationResult = ValidationResult.error.details;
+      SetErrorList(ValidationResult);
+    } else {
+      goToPage("Home");
     }
+  };
 
+  const handelInputs = (e) => {
+    let OldProduct = { ...product };
+    OldProduct[e.target.name] = e.target.value;
+    setProduct(OldProduct);
+  };
 
-    const handleSubmit =async (e) =>{
-        e.preventDefault()
-        let name =e.target.name.value
-        let price =e.target.price.value
-        let category =e.target.category.value
-        let description =e.target.description.value
-        // let getImages =fileSelectedHandler
-        let product = {name ,price,category,description,images}
-        setProduct(product)
-        
-    }
+  function ValidationForm() {
+    const schema = Joi.object({
+      name: Joi.string(),
+      price: Joi.number().min(1),
+      category: Joi.number().min(1)
+      //   Email: Joi.string()
+      //     .required()
+      //     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
+      //   Password: Joi.string().required().pattern(new RegExp("^[a-zA-z0-9]{8,}$"))
+    });
+    return schema.validate(product, { abortEarly: false });
+  }
 
+  useEffect(() => {
+    insertPost();
+  }, [product]);
 
-    const insertPost = async ()=>{
-        let product1 =  product;
-        axios.defaults.withCredentials = true;
-        return axios.post("http://localhost:4000/product" , product1 ).then(res => res.data._id)
-        .then(id => navigate(`/product/${id}/show`, { replace: true }))
-        .catch(err => console.log(err))
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let name = e.target.name.value;
+    let price = e.target.price.value;
+    let category = e.target.category.value;
+    let description = e.target.description.value;
+    // let getImages =fileSelectedHandler
+    let product = { name, price, category, description, images };
+    setProduct(product);
+  };
 
+  const insertPost = async () => {
+    let product1 = product;
+    axios.defaults.withCredentials = true;
+    return axios
+      .post("http://localhost:4000/product", product1)
+      .then((res) =>
+        navigate(`/product/${res.data._id}/show`, { replace: true })
+      )
+      .catch((err) => console.log(err));
+  };
 
+  if (!currentUser) return <Navigate to="../../login" />;
 
-    return (
-        <div>{currentUser ? 
-        <div>
-            <div className='container postContianer mt-5 mb-5 w-50'>
-            <div className='post-header'><h1>Create a new product</h1></div>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="nameInput" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="nameInput" name='name'/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="priceInput" className="form-label">Price</label>
-                    <input type="number" className="form-control" id="priceInput" name='price'/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="categoryInput" className="form-label">Category</label>
-                    <input type="text" className="form-control" id="categoryInput" name="category"/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="descriptionInput" className="form-label">Description</label>
-                    <input type="text" className="form-control" id="descriptionInput" name="description"/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="imageInput" className="form-label">Images</label>
-                    <input type="file" className="form-control" id="imageInput" 
-                    multiple accept='image/jpeg , image/png , image/jpg' 
-                    onChange={fileSelectedHandler}/>
-                </div>
-                <div className='text-center'>
-                    <button type="submit" className="Button formButton">Create</button>
-                </div>
-            </form>
-            </div>
+  return (
+    <div>
+      <div className="container postContianer mt-5 mb-5 w-50">
+        <div className="post-header">
+          <h1>Create a new product</h1>
         </div>
-        :<Navigate to='../../login'/>}
-        </div> 
-    );
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="nameInput" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="nameInput"
+              name="name"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="priceInput" className="form-label">
+              Price
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="priceInput"
+              name="price"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="categoryInput" className="form-label">
+              Category
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="categoryInput"
+              name="category"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="descriptionInput" className="form-label">
+              Description
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="descriptionInput"
+              name="description"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="imageInput" className="form-label">
+              Images
+            </label>
+            <input
+              type="file"
+              className="form-control"
+              id="imageInput"
+              multiple
+              accept="image/jpeg , image/png , image/jpg"
+              onChange={fileSelectedHandler}
+            />
+          </div>
+          <div className="text-center">
+            <button type="submit" className="Button formButton">
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default AddProduct;
-
